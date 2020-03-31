42fe4181b31017c07a8659e7d50a5857c19ef2b0
sergey_spynu <sspynu@tsconsulting.com>
2020-03-31 20:08:31 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			cnt: 0,
+		}
+	}
+
+	handleClickButton = (e) => {
+		const { target } = e;
+		let { cnt } = this.state;
+
+		if (target.name == "up") {
+			cnt++;
+		} else {
+			cnt--;
+		}
+		this.setState({ cnt });
+	};
+
+	render() {
+
+	  return (
+	    <div className="App">
+	      <header className="App-header">
+		      <h1>Cnt = { this.state.cnt }</h1>
+	        <p>
+		        <input name="up" type="button" onClick={ this.handleClickButton } value="Вверхи" /><br />
+		        <input name="down" type="button" onClick={ this.handleClickButton } value="Вниз" /><br />
+
+	          Edit <code>src/App.js</code> and save to reload.
+	        </p>
+	        <a
+	          className="App-link"
+	          href="https://reactjs.org"
+	          target="_blank"
+	          rel="noopener noreferrer"
+	        >
+	          Learn React
+	        </a>
+	      </header>
+	    </div>
+	  );
+
+	}
+}
 export default App;
